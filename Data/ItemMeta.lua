ad7b311da83948349ab820a91b2015fb70d654bb
adawddscasd <157565557+adasdadwasdsc@users.noreply.github.com>
2025-09-22 00:29:20 -0400

    Update ItemMeta.lua

--- Data/ItemMeta.lua
+++ Data/ItemMeta.lua
@@ -1,4 +1,71 @@
-return require(script.Parent.CSV)([[item_id,name,sell_price,bag_kind,battle_kind,desc
+local function parseCSV(str, export_md)
+    if not export_md then
+        export_md = {}
+    end
+    
+    local keys = {}
+    local line1start, line1end = str:find('^[^\n]+\n')
+    for key in str:sub(line1start, line1end-1):gmatch('[^,]+') do
+        table.insert(keys, key)
+    end
+    local data = {}
+    for line in str:sub(line1end+1):gmatch('[^\n]+') do
+        local entry = {}
+        local empty = true
+        local key = 1
+        local charindex = 1
+        while charindex <= #line do
+            local char = line:sub(charindex, charindex)
+            if char == ',' then
+                key = key + 1
+                charindex = charindex + 1
+            else
+                local _, e, value = line:find('([^,]+)', charindex)
+                if char == '"' then
+                    _, e, value = line:find('([^"]+)', charindex)
+                    e = e + 1
+                    while line:sub(e, e+1) == '""' do
+                        local _, newE, v = line:find('([^"]+)', e+2)
+                        if newE then
+                            e = newE + 1
+                        else
+                            e = e + 2
+                        end
+                        value = value .. '"' .. (v or '')
+                    end
+                end
+                if e and value then
+                    local keyname = keys[key]
+                    if (not export_md.exclude or not export_md.exclude[keyname]) and (not export_md.include or export_md.include[keyname]) then
+                        value = tonumber(value) or value
+                        if export_md.map and export_md.map[keyname] then
+                            value = export_md.map[keyname](value)
+                        end
+                        if value ~= nil then
+                            entry[keyname] = value
+                            empty = false
+                        end
+                    end
+                    key = key + 1
+                    charindex = e + 2
+                end
+            end
+        end
+        if not empty then
+            table.insert(data, entry)
+            if _debug then
+                local s = ''
+                for key, value in pairs(entry) do
+                    s = s .. key .. ' = ' .. tostring(value) .. ', '
+                end
+                print(s)
+            end
+        end
+    end
+    return data
+end
+
+local csvData = [[item_id,name,sell_price,bag_kind,battle_kind,desc
 1,Master Ball,,3,1,"The best Pok[e'] Ball with the ultimate level of performance. With it, you will catch any wild pokemon without fail."
 2,Ultra Ball,600,3,1,An ultra-high-performance Pok[e'] Ball that provides a higher success rate for catching pokemon than a Great Ball.
 3,Great Ball,300,3,1,"A good, high-performance Pok[e'] Ball that provides a higher pokemon catch rate than a standard Pok[e'] Ball can."
@@ -686,9 +753,9 @@ return require(script.Parent.CSV)([[item_id,name,sell_price,bag_kind,battle_kind
 796,Psychium Z,,6,,It converts Z-Power into crystals that upgrade Psychic-type moves to Psychic-type Z-Moves.
 797,Rockium Z,,6,,It converts Z-Power into crystals that upgrade Rock-type moves to Rock-type Z-Moves.
 798,Steelium Z,,6,,It converts Z-Power into crystals that upgrade Steel-type moves to Steel-type Z-Moves.
-801,Auspicious Armor ,,5,,"A peculiar set of armor that can make a certain species of Pokémon evolve. Auspicious wishes live within it."
-800,Malicious Armor ,,5,,"A peculiar set of armor that can make a certain species of Pokémon evolve. Malicious will lurks within it."
-799,Marshadium Z,,6,,It is a Pokémon-specific Z-Crystal associated with Marshadow and allows it to upgrade its Spectral Thief into the special Z-Move Soul-Stealing 7-Star Strike.
+801,Auspicious Armor ,,5,,"A peculiar set of armor that can make a certain species of PokÃ©mon evolve. Auspicious wishes live within it."
+800,Malicious Armor ,,5,,"A peculiar set of armor that can make a certain species of PokÃ©mon evolve. Malicious will lurks within it."
+799,Marshadium Z,,6,,It is a PokÃ©mon-specific Z-Crystal associated with Marshadow and allows it to upgrade its Spectral Thief into the special Z-Move Soul-Stealing 7-Star Strike.
 783,Epineshroom,,5,,"A meal prepared by Cooke which, when eaten, would fill you with enough adrenaline to punch through a wall!"
 784,Voltaic Ticket,,5,,"With this ticket, Salty Sam will sail you to the Voltaic Island."
 785,Frigid Ticket,,5,,"With this ticket, Salty Sam will sail you to the Frigid Island."
@@ -727,6 +794,9 @@ return require(script.Parent.CSV)([[item_id,name,sell_price,bag_kind,battle_kind
 759,Zygarde Cube ,,5,,"A Reassembly Unit can use the Cells and Cores in the Zygarde Cube to assemble a new Zygarde."
 760,Sky Pillar Ticket,,5,,"With this ticket, Salty Sam will sail you to the Sky Pillar Island."
 761,Sewer Keycard,,5,,A keycard that opens the lab thats under the Pok[e'] Ball Emporium.
-762,Syrupy Apple,,1,,"A peculiar apple that can make a certain species of Pokémon evolve. It's exceptionally syrupy."
+762,Syrupy Apple,,1,,"A peculiar apple that can make a certain species of PokÃ©mon evolve. It's exceptionally syrupy."
 763,Metal Alloy,,1,,"A peculiar metal that can make certain species of Pok[e']mon evolve. It is composed of many layers."
-]])
+
+]]
+
+return parseCSV(csvData)
